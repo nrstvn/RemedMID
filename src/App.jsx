ca1038ca09dc30f1086fbb550aef0b251e06ab6b
Devan Prabowo <s11810160@student.unklab.ac.id>
2023-11-24 22:24:54 +0800

    Firebase Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./assets/css/style.css";
 import Homepages from "./pages/Homepages";
+import "./firebase_setup/firebase";
 function App() {
   return <Homepages />;
 }
---
+++ src/firebase_setup/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAK74aN29a0jZgZrISjYI5wTkQNiwo6B5A",
+  authDomain: "devan-prabowo-cv.firebaseapp.com",
+  projectId: "devan-prabowo-cv",
+  storageBucket: "devan-prabowo-cv.appspot.com",
+  messagingSenderId: "605149723946",
+  appId: "1:605149723946:web:6424cbb6c366235d357253"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
